29534ca3d49b047565f1060b1ad1ef8e66289eb0
rzipprich <ryan.zipprich@gmail.com>
2023-06-27 00:00:19 -0500

    Added mobile nav button

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
-function onNavButtonClick() {
-  alert("test");
-  document.body.style.background = "black";
+const nav = document.querySelector('nav');
+const navButton = document.querySelector('nav button');
+const navLinks = document.querySelectorAll('nav a');
+const mainContent = document.querySelector('.main-content');
+
+function showNav() {
+  nav.classList.add('full-fixed-screen');
+  navButton.classList.add('hide');
+  for(let link of navLinks) {
+      link.classList.add('show');
+  }
+  mainContent.classList.add('hide');
 }
