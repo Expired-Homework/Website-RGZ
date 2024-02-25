d93544d5fb367d1e7a94e87bca6f6b0299a8d346
rzipprich <ryan.zipprich@gmail.com>
2024-02-24 22:14:55 -0600

    Changed folder structure

--- public/darkMode.js
+++ public/darkMode.js
@@ -5,13 +5,14 @@ const darkModeIcon = document.querySelector("#dark-mode-toggle > img");
 const enableDarkMode = () => {
   document.body.classList.add("dark-mode");
   localStorage.setItem("darkMode", "enabled");
-  darkModeIcon.src = "./assets/icons/night-mode-light.png";
+  darkModeIcon.src = document.head.baseURI + "/../assets/icons/night-mode.png";
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove("dark-mode");
   localStorage.setItem("darkMode", "disabled");
-  darkModeIcon.src = "./assets/icons/night-mode.png";
+  console.log(document.head.baseURI);
+  darkModeIcon.src = document.head.baseURI + "/../assets/icons/night-mode.png";
 };
 
 if (darkModeSetting == "enabled") {
